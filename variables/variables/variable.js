2ba56d593af667fc9dafcda1d8898c78c39e173c
LaraibZahra <167404067+laraibzara@users.noreply.github.com>
2024-06-21 17:37:24 +0500

    Add files via upload

---
+++ variables/variables/variable.js
@@ -0,0 +1,21 @@
+//VARIABLE
+var Fname = "Laraib";
+var Lname = "Ali";
+var age = 25;
+console.log(Fname, Lname, age);
+//SPECIAL SINGN( $, _ , NUMBER )
+var First_Name = "Laraib_Ali";
+var $sir = "Bilal";
+var _School = "GAI";
+var students5 = 100;
+//TEMPLATE LITERALS
+console.log("".concat(First_Name, " ").concat($sir, " ").concat(_School, " ").concat(students5));
+//CONST ( same varaible not Declaird more time ) 
+var colour = "Red";
+var box = "colour box";
+console.log(colour + " " + box);
+//PREMITIVE DATA TYPES
+var nbox = 20;
+var myName = "laraib";
+var isold = true;
+console.log(nbox, myName, isold);

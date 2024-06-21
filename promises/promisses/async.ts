2a493e475e2d0282b2bef11a845aa95198da3f6b
LaraibZahra <167404067+laraibzara@users.noreply.github.com>
2024-06-21 17:41:10 +0500

    Add files via upload

---
+++ promises/promisses/async.ts
@@ -0,0 +1,34 @@
+// promiss
+// Key concept
+//promiss has three types
+//1 pending(initial state)
+//2 fulfill(complet / resolves)
+//3 Rejected(operation failed / rejected)
+// Basic syntex of promiss/ simple way to code a promiss
+// let promiss=newPromiss(function(){})
+// we make a simple promiss programm
+
+
+let pubG
+pubG="we want AC envernoment", "we want to eat biryani" 
+console.log(pubG)
+function makePizza(){
+    return "your pizza is ready"
+}
+let order = makePizza()
+console.log(order)
+
+
+console.log("------------------------------");
+
+let wedding
+wedding="parrty is ready","bridal is looking beautyful","lunch is very testy"
+console.log(wedding)
+function makeBridal(){
+    return "bridal is ready"
+
+}
+let order2 = makeBridal()
+console.log(order2);
+
+
